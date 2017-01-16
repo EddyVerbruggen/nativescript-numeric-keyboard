9e19c059faf5b41872ed9f58463145a3d7d3e06e
EddyVerbruggen <eddyverbruggen@gmail.com>
2017-01-16 20:43:57 +0100

    Bugfixes, Angular support, programmatic support

---
+++ angular/nativescript-numeric-keyboard-directives.ts
@@ -0,0 +1,16 @@
+import * as elementRegistryModule from "nativescript-angular/element-registry";
+import { Directive } from '@angular/core';
+
+import { NumericKeyboardView } from "nativescript-numeric-keyboard";
+
+@Directive({
+  selector: "NumericKeyboard"
+})
+
+export class NumericKeyboardDirective {
+  constructor() { }
+}
+
+export const NSNUMKEY_DIRECTIVES = [ NumericKeyboardDirective ];
+
+elementRegistryModule.registerElement("NumericKeyboard", () => NumericKeyboardView);
--- numeric-keyboard.ios.ts
+++ numeric-keyboard.ios.ts
@@ -1,43 +1,110 @@
-import { CommonNumericKeyboardView } from "./numeric-keyboard.common";
+import { NumericKeyboardApi, NumericKeyboardOptions } from "./numeric-keyboard.common";
+import { TextView } from "ui/text-view";
+import { Color } from "color";
 
-declare const MMNumberKeyboard, CGRectZero, UIInputViewStyleDefault,
-    NSLocale, MMNumberKeyboardDelegate, MMNumberKeyboardButtonStyleGray : any;
+declare const MMNumberKeyboard, CGRectZero, UIInputViewStyleDefault, UIInputViewStyleKeyboard,
+    NSLocale, MMNumberKeyboardDelegate, MMNumberKeyboardButtonStyleGray, NSLineBreakByTruncatingHead: any;
 
-export class NumericKeyboard extends CommonNumericKeyboardView {
+export class NumericKeyboard implements NumericKeyboardApi {
+  private _keyboardDelegate: MMNumberKeyboardDelegateImpl;
+  private _keyboard: any;
+
+  decorate(args?: NumericKeyboardOptions): Promise<any> {
+    const that = this;
+
+    return new Promise((resolve, reject) => {
+      if (!args || !args.textView) {
+        reject("Setting the 'textView' property is mandatory.");
+        return;
+      }
+
+      let nslocale = null;
+      if (args.locale) {
+        nslocale = NSLocale.localeWithLocaleIdentifier(args.locale);
+      }
+      this._keyboard = MMNumberKeyboard.alloc().initWithFrameInputViewStyleLocale(CGRectZero, UIInputViewStyleKeyboard, nslocale);
+
+      if (args.returnKeyTitle) {
+        this._keyboard.returnKeyTitle = args.returnKeyTitle;
+      }
+
+      this._keyboard.allowsDecimalPoint = !args.noDecimals;
+
+      // not exposing this just yet (not too useful)
+      // keyboard.returnKeyButtonStyle = MMNumberKeyboardButtonStyleDone; // (Done = default, there's also White and Gray)
+
+      if (args.noReturnKey) {
+        this._keyboardDelegate = MMNumberKeyboardDelegateImpl.initWithOwner(new WeakRef(this));
+        this._keyboardDelegate.setCallback(function () {
+          return false
+        });
+        this._keyboard.delegate = this._keyboardDelegate;
+        if (!args.returnKeyTitle) {
+          this._keyboard.returnKeyTitle = " ";
+          this._keyboard.returnKeyButtonStyle = MMNumberKeyboardButtonStyleGray; // (Done (blue) = default, there's also White)
+        }
+      }
+
+      args.textView.ios.inputView = this._keyboard; // kills events firing..
+
+      // if not set by the user make it transparent (just like regular TextFields are)
+      if (!args.textView.backgroundColor) {
+        args.textView.backgroundColor = new Color("transparent");
+      }
+
+      resolve();
+    });
+  }
+}
+
+export class NumericKeyboardView extends TextView {
   private _returnKeyTitle: string;
   private _locale: string;
   private _noDecimals: boolean;
   private _noReturnKey: boolean;
-  private _ios: any; // = MMNumberKeyboard;
-
-  public onLoaded() {
-    let nslocale = null;
-    if (this._locale) {
-      nslocale = NSLocale.localeWithLocaleIdentifier(this._locale);
-    }
-    const keyboard = MMNumberKeyboard.alloc().initWithFrameInputViewStyleLocale(CGRectZero, UIInputViewStyleDefault, nslocale);
-
-    if (this._returnKeyTitle) {
-      keyboard.returnKeyTitle = this._returnKeyTitle;
-    }
-
-    keyboard.allowsDecimalPoint = !this._noDecimals;
-
-    // not exposing this just yet (not too useful)
-    // keyboard.returnKeyButtonStyle = MMNumberKeyboardButtonStyleDone; // (Done = default, there's also White and Gray)
-
-    if (this._noReturnKey) {
-      keyboard.delegate = MMNumberKeyboardDelegateImpl.new().initWithCallback(function() { return false});
-      if (!this._returnKeyTitle) {
-        keyboard.returnKeyTitle = " ";
-        keyboard.returnKeyButtonStyle = MMNumberKeyboardButtonStyleGray; // (Done (blue) = default, there's also White)
-      }
-    }
-
-    this._ios.inputView = keyboard;
-  }
+  private _keyboardDelegate: MMNumberKeyboardDelegateImpl = null;
+  private _ios: any;
+  private _loaded: boolean = false;
+  private _keyboard: any;
+  /* MMNumberKeyboard */
 
   get ios(): any {
+    if (!this._loaded) {
+      this._loaded = true;
+      let nslocale = null;
+      if (this._locale) {
+        nslocale = NSLocale.localeWithLocaleIdentifier(this._locale);
+      }
+      this._keyboard = MMNumberKeyboard.alloc().initWithFrameInputViewStyleLocale(CGRectZero, UIInputViewStyleDefault, nslocale);
+
+      if (this._returnKeyTitle) {
+        this._keyboard.returnKeyTitle = this._returnKeyTitle;
+      }
+      this._keyboard.allowsDecimalPoint = !this._noDecimals;
+
+      if (this._noReturnKey) {
+        this._keyboardDelegate = MMNumberKeyboardDelegateImpl.initWithOwner(new WeakRef(this));
+        this._keyboardDelegate.setCallback(function () {
+          return false
+        });
+        this._keyboard.delegate = this._keyboardDelegate;
+        if (!this._returnKeyTitle) {
+          this._keyboard.returnKeyTitle = " ";
+          this._keyboard.returnKeyButtonStyle = MMNumberKeyboardButtonStyleGray; // (Done (blue) = default, there's also White)
+        }
+      }
+
+      this._ios.textContainer.maximumNumberOfLines = 1;
+      this._ios.textContainer.lineBreakMode = NSLineBreakByTruncatingHead;
+      // not exposing this just yet (not too useful)
+      // keyboard.returnKeyButtonStyle = MMNumberKeyboardButtonStyleDone; // (Done = default, there's also White and Gray)
+      this._ios.inputView = this._keyboard;
+
+      // if not set by the user make it transparent (just like regular TextFields are)
+      if (!this.backgroundColor) {
+        this.backgroundColor = new Color("transparent");
+      }
+    }
     return this._ios;
   }
 
@@ -66,18 +133,32 @@ export class NumericKeyboard extends CommonNumericKeyboardView {
 class MMNumberKeyboardDelegateImpl extends NSObject /* implements MMNumberKeyboardDelegate */ {
   public static ObjCProtocols = [MMNumberKeyboardDelegate];
 
-  static new(): MMNumberKeyboardDelegateImpl {
-    return <MMNumberKeyboardDelegateImpl>super.new();
+  private _owner: WeakRef<any>;
+
+  public static initWithOwner(owner: WeakRef<any>): MMNumberKeyboardDelegateImpl {
+    let delegate = <MMNumberKeyboardDelegateImpl>MMNumberKeyboardDelegateImpl.new();
+    delegate._owner = owner;
+    return delegate;
   }
 
   private _onReturnKeyPressedCallback: (keyboard) => void;
 
-  public initWithCallback(callback: (keyboard?) => boolean): MMNumberKeyboardDelegateImpl {
+  public setCallback(callback: (keyboard?) => boolean): void {
     this._onReturnKeyPressedCallback = callback;
-    return this;
   }
 
+  /* no need for these yet
+   public numberKeyboardShouldInsertText(keyboard, text) {
+   return true;
+   }
+
+   public numberKeyboardShouldDeleteBackward(keyboard) {
+   return true;
+   }
+   */
+
   public numberKeyboardShouldReturn(keyboard) {
+    console.log("--- delegate: numberKeyboardShouldReturn, this._onReturnKeyPressedCallback: " + this._onReturnKeyPressedCallback);
     if (this._onReturnKeyPressedCallback) {
       return this._onReturnKeyPressedCallback(keyboard);
     } else {
