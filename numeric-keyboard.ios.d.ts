ef27e908197a304332f50ed17dbdf6703f2c8dae
Eddy Verbruggen <eddyverbruggen@gmail.com>
2017-04-03 17:55:19 +0200

    Decimal separator issues #6

--- numeric-keyboard.ios.d.ts
+++ numeric-keyboard.ios.d.ts
@@ -1,11 +1,19 @@
 import { NumericKeyboardApi, NumericKeyboardOptions } from "./numeric-keyboard.common";
 import { TextView } from "ui/text-view";
-export declare class NumericKeyboard implements NumericKeyboardApi {
+export interface TextAndDecimalSeparatorHolder {
+    getDecimalSeparator(): string;
+    getText(): string;
+}
+export declare class NumericKeyboard implements NumericKeyboardApi, TextAndDecimalSeparatorHolder {
     private _keyboardDelegate;
     private _keyboard;
+    private _nativeTextView;
+    private _decimalSep;
+    getDecimalSeparator(): string;
+    getText(): string;
     decorate(args?: NumericKeyboardOptions): Promise<any>;
 }
-export declare class NumericKeyboardView extends TextView {
+export declare class NumericKeyboardView extends TextView implements TextAndDecimalSeparatorHolder {
     private _returnKeyTitle;
     private _locale;
     private _noDecimals;
@@ -15,6 +23,9 @@ export declare class NumericKeyboardView extends TextView {
     _ios: any;
     private _loaded;
     private _keyboard;
+    private _decimalSep;
+    getDecimalSeparator(): string;
+    getText(): string;
     readonly ios: any;
     readonly _nativeView: any;
     returnKeyTitle: string;
